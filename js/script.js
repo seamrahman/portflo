7b3b3151c8e16056936f900d8554023673f0da83
Seam Rahman <80593803+seamrahman@users.noreply.github.com>
2021-03-16 23:52:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//service-slider start//
+$('.service-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: ".left",
+    prevArrow: ".right",
+    centerMode: true,
+    centerPadding: '0',
+});
+//banner-slider start//
+
+//portfolio mixit up start//
+
+var mixer = mixitup('.pro');
+
+//portfolio mixit up end//
+
+//service-slider start//
+$('.test-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+     nextArrow: ".test-left",
+    prevArrow: ".test-right",
+});
